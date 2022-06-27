08632afa20018e6b5d1b9e75fcae6088774921e5
mdadul <emdadulislam580@gmail.com>
2022-06-27 18:01:18 +0600

    Insertion sort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,39 +1,38 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 void insertionSort(int arr[], int n)
 {
-    for (int i = 1; i <=n; i++)
+    for (int i = 1; i <= n; i++)
     {
         int x = arr[i];
-        int j = i-1;
+        int j = i - 1;
 
-        while(j>=1 && arr[j]>x){
-            arr[j+1] = arr[j];
+        while (j >= 1 && arr[j] > x)
+        {
+            arr[j + 1] = arr[j];
             j--;
         }
-        arr[j+1]=x;
+        arr[j + 1] = x;
     }
-    
 }
 void show(int arr[], int n)
 {
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
-        cout<<arr[i]<<" ";
-
+        cout << arr[i] << " ";
     }
-    cout<<endl;
+    cout << endl;
 }
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    int arr[n+10];
-    for(int i=0; i<n; i++) cin>>arr[i];
+    cin >> n;
+    int arr[n + 10];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
 
-    insertionSort(arr,n);
+    insertionSort(arr, n);
 
-    show(arr,n);
-    
-
+    show(arr, n);
 }
