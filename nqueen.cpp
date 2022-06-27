42b9053a8125156a4937a2afed59f57083aa913b
mdadul <emdadulislam580@gmail.com>
2022-06-27 18:03:22 +0600

    NQueen Problem

--- nqueen.cpp
+++ nqueen.cpp
@@ -2,36 +2,36 @@
 using namespace std;
 
 int queen[20];
-int column[20], diagonal1[40],diagonal2[40];
+int column[20], diagonal1[40], diagonal2[40];
 
 void nqueen(int at, int n)
 {
-    if(at== n+1)
+    if (at == n + 1)
     {
-        cout<<"(row,column) = ";
-        for(int i=1; i<=n; i++)
+        cout << "(row,column) = ";
+        for (int i = 1; i <= n; i++)
         {
-            cout<<i<<" "<<queen[i];
+            cout << i << " " << queen[i];
         }
-        cout<<endl;
+        cout << endl;
         return;
     }
 
     for (int i = 1; i <= n; i++)
     {
-        if(column[i] || diagonal1[i+at] || digonal2[n+1-at])  continue;
+        if (column[i] || diagonal1[i + at] || digonal2[n + 1 - at])
+            continue;
 
-        queen[at]= i;
+        queen[at] = i;
 
-        column[i] = diagonal1[i+at] = diagonal2[n+1-at] = 1;
+        column[i] = diagonal1[i + at] = diagonal2[n + 1 - at] = 1;
 
-        nqueen(at+1,n);
-        column[i] = diagonal1[i+at] = diagonal2[n+i-at] = 0;  
+        nqueen(at + 1, n);
+        column[i] = diagonal1[i + at] = diagonal2[n + i - at] = 0;
     }
-    
 }
-int main(){
+int main()
+{
 
-    nqueen(1,8) // 8 queen problem
-
+    nqueen(1, 8) // 8 queen problem
 }
